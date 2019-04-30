00c842ace34567546d98ec045c0aa4294197e55f
Matt Paul <matthew.paul.2006@gmail.com>
2019-04-30 12:21:40 -0400

    MACS input changed to BED from BAM
    BED files are now the MACS input, as the BAM file structure is not maintained following macs filtering earlier on in the pipeline. This makes it unrecognizable to MACS.

--- src/doMACS2.s
+++ src/doMACS2.s
@@ -13,7 +13,7 @@ params=`sed -n ${val}p forMacs.txt`
 read chip input name kind <<< $params
 
 echo "Predicting fragment size for file $chip..."
-macs2 predictd -i $chip &> ${val}.txt
+macs2 predictd -i ${chip}.bed &> ${val}.txt
 fragment_size=$(grep -E -o 'predicted fragment length is [[:digit:]]+' ${val}.txt | grep -E -o '[[:digit:]]+')
 echo "fragment_size: $fragment_size"
 
@@ -24,10 +24,10 @@ echo "Calling peaks..."
 
 if [[ $kind == "single" ]]; then
   echo "${name} is single: q = 0.05"
-  macs2 callpeak -t $chip -c $input --outdir MACSoutput -f BAM -g ce -n $name -q 0.05 --nomodel --extsize $fragment_size
+  macs2 callpeak -t ${chip}.bed -c ${input}.bed --outdir MACSoutput -f BED -g ce -n $name -q 0.05 --nomodel --extsize $fragment_size
 else
   echo "${name} is merged: q = 0.01"
-  macs2 callpeak -t $chip -c $input --outdir MACSoutput -f BAM -g ce -n $name -q 0.01 --nomodel --extsize $fragment_size
+  macs2 callpeak -t ${chip}.bed -c ${input}.bed --outdir MACSoutput -f BED -g ce -n $name -q 0.01 --nomodel --extsize $fragment_size
 fi
 
 exit 0;
